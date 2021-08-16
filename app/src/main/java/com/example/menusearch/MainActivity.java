0a5882ee01ee444884a24a76d4249bd79cccac44
PRIYANKA-Nigam <nigampriyanka042@gmail.com>
2021-08-16 20:16:15 +0530

    menu

---
+++ app/src/main/java/com/example/menusearch/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.menusearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+ListView listView;
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=(ListView)findViewById(R.id.ll);
+        ArrayList<String> adapter1=new ArrayList<>();
+        adapter1.addAll(Arrays.asList(getResources().getStringArray(R.array.my)));
+        adapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,adapter1);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.mun,menu);
+        MenuItem item=menu.findItem(R.id.sea);
+        SearchView searchView=(SearchView)item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}
